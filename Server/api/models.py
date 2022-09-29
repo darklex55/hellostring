47af97d7f51ff0d26b31f7ac44577b77c0f8b847
Alex Karas <darklex55@gmail.com>
2022-09-30 01:33:04 +0300

    Update models.py

--- Server/api/models.py
+++ Server/api/models.py
@@ -4,16 +4,16 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True)
-    password = db.Column(db.String(150))
-    auth_key = db.Column(db.String(100))
-    mail_auth_key = db.Column(db.String(100))
+    password = db.Column(db.String(64))
+    auth_key = db.Column(db.String(64))
+    mail_auth_key = db.Column(db.String(64))
     is_authed = db.Column(db.Boolean, default=False)
     is_privilleged = db.Column(db.Boolean, default=False)
 
 
 class Text_Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    auth_key = db.Column(db.String(100))
+    auth_key = db.Column(db.String(64))
     parameters = db.Column(db.String(200))
-    text = db.Column(db.String(100))
+    text = db.Column(db.String(1000))
     rest = db.Column(db.Boolean, default=False)
