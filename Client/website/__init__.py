f524123d16f4a4d5da0a3be22ec5076f6f0863a0
Alex Karas <darklex55@gmail.com>
2022-09-30 15:02:51 +0300

    Clean up code

--- Client/website/__init__.py
+++ Client/website/__init__.py
@@ -1,17 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 import requests, json
-
-db = SQLAlchemy()
-DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'kara2004'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -21,7 +14,6 @@ def create_app():
 
     from .models import User
 
-    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -41,8 +33,3 @@ def create_app():
             return User(id,'','','','','','')
 
     return app
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database')
